696ed02a2f03316f0f45fc07cc4d1834bc77b5e8
LiNb0in <970732990@qq.com>
2018-07-21 12:36:01 +0800

    Update checkRequiredFiles.js

--- packages/react-dev-utils/checkRequiredFiles.js
+++ packages/react-dev-utils/checkRequiredFiles.js
@@ -9,23 +9,30 @@
 
 var fs = require('fs');
 var path = require('path');
-var chalk = require('chalk');
-
+var chalk = require('chalk'); // 终端输出字体颜色插件
+// 检测引入文件
 function checkRequiredFiles(files) {
-  var currentFilePath;
+  var currentFilePath; // 当前文件路径
   try {
-    files.forEach(filePath => {
-      currentFilePath = filePath;
-      fs.accessSync(filePath, fs.F_OK);
+    files.forEach(filePath => { // 遍历文件路径
+      currentFilePath = filePath; // 设置当前文件路径
+      // fs.accessSync(file, mode)
+      // mode : 
+      //    F_OK 表明文件对于调用进程是可见的
+      //    R_OK 表明文件可被调用进程读取
+      //    W_OK 表明文件可被调用进程写入
+      //    X_OK 表明文件可被调用进程执行
+      fs.accessSync(filePath, fs.F_OK); // 检查失败抛出err, 否则返回undefined
     });
-    return true;
+    return true; // 通过检查
   } catch (err) {
-    var dirName = path.dirname(currentFilePath);
-    var fileName = path.basename(currentFilePath);
-    console.log(chalk.red('Could not find a required file.'));
+    // 如果报错
+    var dirName = path.dirname(currentFilePath); // 返回当前报错的路径如: use/info/name
+    var fileName = path.basename(currentFilePath); // 返回当前报错的文件名如: xxx.html
+    console.log(chalk.red('Could not find a required file.')); 
     console.log(chalk.red('  Name: ') + chalk.cyan(fileName));
     console.log(chalk.red('  Searched in: ') + chalk.cyan(dirName));
-    return false;
+    return false; // 检查不通过
   }
 }
 
